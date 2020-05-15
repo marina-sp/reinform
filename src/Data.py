05614c30088342e730ce6a4c1b00a552facb8c09
marina-sp <aquamarin1995@gmail.com>
2020-05-15 18:46:11 +0200

    parametrize inverse triples in data (not the graph)

--- src/Data.py
+++ src/Data.py
@@ -10,17 +10,6 @@ class Data_loader():
         self.option = option
         self.include_reverse = option.use_inverse
 
-        self.entity2num = None
-        self.num2entity = None
-
-        self.relation2num = None
-        self.num2relation = None
-        self.relation2inv = None
-
-        self.num_relation = 0
-        self.num_entity = 0
-        self.num_operator = 0
-
         data_path = os.path.join(self.option.datadir, self.option.dataset)
 
         self.kg = CustomKG("freebase15k_237")
@@ -28,35 +17,70 @@ class Data_loader():
         self.kg.add_reversed_relations()
         self.kg.add_extra_relations()
 
+        self.load_mappings()
         self.load_data_all(data_path)
 
-    def load_data_all(self, path):
+    def load_mappings(self):
         self.entity2num, self.num2entity = self.kg.entity2idx, self.kg.idx2entity
         self.relation2num, self.num2relation = self.kg.relation2idx, self.kg.idx2relation
-        # todo
-        # assert len(self.entity2num) == len(self.num2entity)
-        # assert len(self.relation2num) == len(self.num2relation)
+        # self._augment_reverse_relation()
+        # self._add_item(self.relation2num, self.num2relation, "Equal")
+        # self._add_item(self.relation2num, self.num2relation, "Pad")
+        # self._add_item(self.relation2num, self.num2relation, "Start")
+        # self._add_item(self.entity2num, self.num2entity, "Pad")
+        # print(self.relation2num)
 
         self.num_relation = len(self.relation2num)
         self.num_entity = len(self.entity2num) + self.kg.reserved_vocab
         print("num_relation", self.num_relation)
-        print("num_entity", self.num_entity)
+        print("num_entity", self.num_entity - self.kg.reserved_vocab)
 
+    ## finilize for different relations and entity embedding sizes
+    #     # map joint Transformer indices for relations and entities to separate mappings
+    #     self.mixed2ent = {v: i for i, v in enumerate(self.kg.entity2idx.values())}
+    #     self.ent2mixed = {v: k for k, v in self.mixed2ent.items()}
+    #     # include reserved relations
+    #     self.mixed2rel = {v: i for i, v in enumerate(self.kg.entity2idx.values())}
+    #     self.rel2mixed = {v: k for k, v in self.mixed2rel.items()}
+    #     # add pad token mapping
+    #     self._add_item(self.mixed2rel, self.rel2mixed, 0)
+    #
+    #     # include reserved tokens for embedding
+    #     self.num_relation = len(self.mixed2ent)
+    #     self.num_entity = len(self.mixed2rel)
+    #     # print data stats without reserved tokens
+    #     print("num_relation", self.num_relation)
+    #     print("num_entity", self.num_entity)
+    #
+    #     # assert len(self.entity2num) == len(self.num2entity)
+    #     # assert len(self.relation2num) == len(self.num2relation)
+    #
+    # def _add_item(self, obj2num, num2obj, item):
+    #     count = len(obj2num)
+    #     obj2num[item] = count
+    #     num2obj[count] = item
+    #
+    #
+
+    def load_data_all(self, path):
         self.data = {}
-        # todo: check inverse relations in data
-        self.data['train'], self.data['inv_train'] = self.load_data("train"), []
-        self.data['valid'], self.data['inv_valid'] = self.load_data("valid"), []
-        self.data['test'],  self.data['inv_test']  = self.load_data("test"), []
 
-        # todo
-        # print("total seen relations", len(self.relation2num) - 3)
-        # print("total seen entities", len(self.entity2num) - 1)
+        self.data['train'], self.data['inv_train'] = self.load_data("train")
+        self.data['valid'], self.data['inv_valid'] = self.load_data("valid")
+        self.data['test'],  self.data['inv_test']  = self.load_data("test")
+
 
     def load_data(self, data):
-        return [[t.h, t.r, t.t] for t in self.kg.triplets[data]]
+        ## inversed triplets in kg are added last
+        split = len(self.kg.triplets[data]) // 2
+        assert split * 2 == len(self.kg.triplets[data])
+        return (
+            [[t.h, t.r, t.t] for t in self.kg.triplets[data][:split]],
+            [[t.h, t.r, t.t] for t in self.kg.triplets[data][split:]]
+        )
 
     def get_data(self, data):
-        out = self.data['data'] + self.data[f'inv_{data}'] if self.include_reverse else self.data[data]
+        out = self.data[data] + self.data[f'inv_{data}'] if self.include_reverse else self.data[data]
         with open(os.path.join(self.option.this_expsdir, f"{data}_log.txt"), "a+", encoding='UTF-8') as f:
             f.write(f"{data} data contains {len(out)} triples\n")
         return np.array(out, dtype=np.int64)
